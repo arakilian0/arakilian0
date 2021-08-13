cf301e39bf37619b5b1394835c37468da8d0acc1
Michael Arakilian <arakilian0@gmail.com>
2021-08-13 13:48:45 -0700

    Create Footer.js

---
+++ comps/Layout/Footer.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import styles from '@styles/module/Layout.module.css'
+
+function Footer() {
+    return(
+        <Fragment>
+            <footer className={styles.footer}>
+                <div>Footer</div>
+            </footer>
+        </Fragment>
+    )
+}
+
+export default Footer
