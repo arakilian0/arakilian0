92b7a9d31063715cc053ed5b9c2310c202feac1f
Michael Arakilian <arakilian0@gmail.com>
2021-08-31 11:18:56 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import { Fragment } from 'react'
 import _Head from '@comps/Head'
 
+import { Container } from 'react-bootstrap'
+
 function HomePage() {
     return(
       <Fragment>
-        <_Head 
-          title="not something else">
-            
-        </_Head>
-        <div>Welcome to my portfolio!</div>
+        <_Head title="not something else" />
+        <Container>
+            <div>Welcome to my portfolio!</div>
+        </Container>
       </Fragment>
     ) 
   }
