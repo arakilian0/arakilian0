a0e05aefbebb5a4b5c8e786c45f0964e7dc1c3a6
Michael Arakilian <arakilian0@gmail.com>
2021-09-02 01:14:46 -0700

    Update mobile layout

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,16 @@ import styles from '@styles/module/page/Home.module.css'
 
 import profilePic from '../public/me.jpg'
 
-function HomePage() {
+function HomePage() { 
     return(
       <Fragment>
         <_Head title="not something else" />
         <Container className="main">
-            <div className={styles.leftMain}>
+            <div className={styles.leftMain} id="leftMain">
               <Image src={profilePic} width="100" height="100" className={styles.profilePic} alt="Picture of the author" />
             </div>
             <div className={styles.rightMain}>
+              <h2>Full Stack Developer</h2>
               It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years.
             </div>
         </Container>
